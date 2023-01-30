ae65f377f298ebeccc4cec9db5b821e4bbf37a29
priyanka chatterjee <chatterjeepriyanka988@gmail.com>
2023-01-30 22:18:18 +0530

    add: header footer in validate php

--- CV_PROJECT/validate.php
+++ CV_PROJECT/validate.php
@@ -1,4 +1,12 @@
-<style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>CERTIFICATE VERIFICATION</title>
+  <style>
 
     label, button {
       font-size: 16px;
@@ -67,19 +75,8 @@ url('data:image/gif;base64,R0lGODlhAQABAPAAAP///////yH5BAAAAAAALAAAAAABAAEAAAICR
       background-size: 100% 2px, 100% 98%, 0 2px;
     }
 }
-
-
 </style>
-
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
-  <title>CERTIFICATE VERIFICATION</title>
 </head>
 <body>
   <!--Header-->
